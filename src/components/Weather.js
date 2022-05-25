d3d4402ab80da6b905d0016a973f1c69e6f7bbfa
Rakesh Dhariwal <rakeshdhariwal61@gmail.com>
2022-05-25 20:43:30 +0530

    Added useEffect in Weather component now can fetch the weather using api. Also added button the view weather of Haryana and Delhi

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,10 +1,42 @@
 import WeatherCard from "./WeatherCard";
-import React from 'react';
+import React, { useCallback } from 'react';
 import WeatherData from "./WeatherData";
-export default function Weather() { 
+import { useEffect, useState } from "react";
+import { Button } from "@mui/material";
+export default function Weather() {
+  const [useTemp, setTemp] = useState(0);
+  const [useCity, setCity] = useState(3099739);
+  const url = `http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/${useCity}?apikey=Uw9BkVdX9esosM8QCKVzJfxuGrv8GC9J`;
+
+            /// this is the normal updateWeather function which cause dependicies error in useEffect 
+            // bcs of its complex structrue as its also inlcude url which changes on time
+  // const updateWeather = async ()=>{
+  //   let data = await fetch(url);
+  //   let parsedData = await data.json();
+  //   console.log(parsedData);
+  //   setTemp(parsedData[0].Temperature.Value)
+  // }
+
+  // url fetch function with useCallBack to remove the complex dependicies of useEffect
+  const updateWeather = useCallback(async () => {
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    setTemp(parsedData[0].Temperature.Value)
+  }, [url])
+
+  // handle click funstion to change the city
+  const changeState = () => {
+    setCity(useCity === 202396 ? 3099739 : 202396);
+  }
+
+  // useEffect hooks which runs on the change of url
+  useEffect(() => {
+    updateWeather();
+  }, [url, updateWeather]);
   return (
-      <>
-      <WeatherCard temp = {WeatherData[0].Temperature.Value} type = {WeatherData[0].IconPhrase}/>
-      </>
+    <>
+      <WeatherCard temp={useTemp} type={WeatherData[0].IconPhrase} />
+      <Button onClick={changeState}>changeState</Button>
+    </>
   )
 }
